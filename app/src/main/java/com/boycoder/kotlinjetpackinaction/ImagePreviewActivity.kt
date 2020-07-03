fc60c03f310a9e14897a97a3547896accf91aeb6
carlshen <xiangfeng.shen@outlook.com>
2020-07-03 21:15:26 +0800

    move java to kotlin

---
+++ app/src/main/java/com/boycoder/kotlinjetpackinaction/ImagePreviewActivity.kt
@@ -0,0 +1,24 @@
+package com.boycoder.kotlinjetpackinaction;
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_image_preview.*
+
+class ImagePreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_preview);
+
+        val intent: Intent = getIntent();
+        val url: String = intent.getStringExtra(MainActivity.EXTRA_PHOTO);
+        if (!TextUtils.isEmpty(url)) {
+            Glide.with(this@ImagePreviewActivity).load(url).into(imagePreview);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/boycoder/kotlinjetpackinaction/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.boycoder.kotlinjetpackinaction;
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.boycoder.kotlinjetpackinaction.entity.User
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        val TAG = "Main";
+        val EXTRA_PHOTO = "photo";
+    }
+
+    private lateinit var stringRequest: StringRequest;
+    private lateinit var requestQueue: RequestQueue;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private fun init() {
+        display(User.CACHE_RESPONSE);
+        requestOnlineInfo();
+    }
+
+    private fun requestOnlineInfo() {
+        requestQueue = Volley.newRequestQueue(this);
+        val url ="https://api.github.com/users/JakeWharton";
+        stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener<String>() {
+                    @Override
+                    fun onResponse(response: String) {
+                        display(response);
+                    }
+                }, Response.ErrorListener() {
+            @Override
+            fun onErrorResponse(error: VolleyError) {
+                Toast.makeText(this@MainActivity, error?.message, Toast.LENGTH_SHORT).show();
+            }
+        });
+        stringRequest.setTag(TAG);
+        requestQueue.add(stringRequest);
+    }
+
+    private fun display(response: String?) {
+        if (TextUtils.isEmpty(response)) { return; }
+
+        val gson: Gson = Gson();
+        val user: User = gson.fromJson(response, User::class.java);
+        user?.apply {
+            Glide.with(this@MainActivity).load("file:///android_asset/bless.gif").into(gif);
+            Glide.with(this@MainActivity).load(user.avatar_url).apply(RequestOptions.circleCropTransform()).into(image);
+            username.text = name;
+            this@MainActivity.company.text = company;
+            website.text = blog
+
+            image!!.setOnClickListener(View.OnClickListener() {
+                @Override
+                fun onClick(v: View?) {
+                    gotoImagePreviewActivity(user)
+                }
+            });
+        }
+    }
+
+    private fun gotoImagePreviewActivity(user: User) {
+        val intent: Intent = Intent(this, ImagePreviewActivity::class.java);
+        intent.putExtra(EXTRA_PHOTO, user.avatar_url);
+        startActivity(intent);
+    }
+
+    override fun onStop () {
+        super.onStop();
+        if (requestQueue != null) {
+            requestQueue.cancelAll(TAG);
+        }
+    }
+}
